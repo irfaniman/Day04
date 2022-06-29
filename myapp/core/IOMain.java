8016ada227635c0780273e65ae66c93d745e13f8
Irfan <irfanimanabub@gmail.com>
2022-06-29 13:19:06 +0800

    29 June

--- myapp/core/IOMain.java
+++ myapp/core/IOMain.java
@@ -1,30 +1,31 @@
 package myapp.core;
 
+import java.io.File;
 import java.io.FileInputStream;
-
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+// java IOMain <src> <dest>
+// java IOMain myfile myfile.dup
+
 public class IOMain {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        String srcFile = args [0];
-        String dstFile = args [1];
+        String srcFile = args[0];
+        String dstFile = args[1];
 
-        // 1k byte buffer
-        byte [] buff = new byte [1024];
+        // 1K byte buffer
+        byte[] buff = new byte[1024 * 16];
         int size = 0;
         try {
-            InputStream is = new FileInputStream(srcFile);
-            System.out.printf("%s file opened", srcFile);
+            InputStream is = new FileInputStream(srcFile);  //srcFile
+            System.out.printf("%s file opened\n", srcFile); //srcFile
 
             OutputStream os = new FileOutputStream(dstFile);
-        
 
             while (size >= 0) {
                 size = is.read(buff);
@@ -32,16 +33,17 @@ public class IOMain {
                 if (size > 0)
                     os.write(buff, 0, size);
             }
+
             is.close();
+
             os.flush();
-            os.close();
+            os.close(); 
 
         } catch (FileNotFoundException ex) {
-            System.err.printf("Error %s\n", ex.getMessage());
+            System.err.printf("Error: %s\n", ex.getMessage());
         } catch (IOException ex) {
-            System.err.printf("Error %s\n", ex.getMessage());
+            System.err.printf("Error: %s\n", ex.getMessage());
         }
-
     }
-   
+    
 }
--- myapp/core/MapMain.java
+++ myapp/core/MapMain.java
@@ -1,49 +1,36 @@
 package myapp.core;
 
 import java.util.Map;
-import java.util.HashMap;
 import java.util.Set;
-import java.util.Collection; 
-import java.util.List;
-import java.util.LinkedList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class MapMain {
-    
-    public static void main (String[] args) {
-
-        List<String> names = new LinkedList<>();
-        names.add("fred");
-        names.add("barney");
-        names.add("wilma");
-        names.add("betty");
-
-        Map<String, BankAccount> accts = new HashMap<>();
+    public static void main(String[] args) {
 
         BankAccount fred = new BankAccount("fred");
         BankAccount barney = new BankAccount("barney");
         BankAccount wilma = new BankAccount("wilma");
         BankAccount betty = new BankAccount("betty");
 
-        // create a simple map
-        //Map<String, BankAccount> accts = new HashMap<>();
-        accts.put(fred.getAccountID(), fred);
-        accts.put(barney.getAccountID(), barney);
-        accts.put(wilma.getAccountID(), wilma);
-        accts.put(betty.getAccountID(), betty);
+        // Create a simple Map
+        Map<String, BankAccount> accts = new HashMap<>();
+        accts.put(fred.getAccountId(), fred);
+        accts.put(barney.getAccountId(), barney);
+        accts.put(wilma.getAccountId(), wilma);
+        accts.put(betty.getAccountId(), betty);
 
         System.out.printf("size: %d\n", accts.size());
-        System.out.printf("has fred: %b\n", accts.containsKey(fred.getAccountID()));
+        System.out.printf("has fred: %b\n", accts.containsKey(fred.getAccountId()));
         System.out.printf("has pebbles: %b\n", accts.containsKey("pebbles"));
 
         Set<String> keys = accts.keySet();
-        //Collection<BankAccount> values = accts.values();
-
-        BankAccount acct;
-        for (String acctID: keys) {
-            acct = accts.get(acctID);
-            System.out.printf("acctid = %s, name = %s\n", acctID, acct.getName());
-        }
-
+        Collection<BankAccount> values = accts.values();
         
+        BankAccount acct;
+        for (String acctId : keys) {
+            acct = accts.get(acctId);
+            System.out.printf("acctid = %s, name = %s\n", acctId, acct.getName());
+        } 
     }
-}
+} 
